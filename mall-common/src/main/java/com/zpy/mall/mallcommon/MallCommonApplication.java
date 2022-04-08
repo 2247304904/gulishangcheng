f7a64842ba98e73c85571d5167f8540f10908b88
张鹏宇 <2247304904@qq.com>
2022-04-08 21:28:15 +0800

    2020-4-8 项目结构修改。。。。

---
+++ mall-common/src/main/java/com/zpy/mall/mallcommon/MallCommonApplication.java
@@ -0,0 +1,13 @@
+package com.zpy.mall.mallcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCommonApplication.class, args);
+    }
+
+}
