52fc5e9d2c408feaf5ad244f28c163763820e27a
张鹏宇 <2247304904@qq.com>
2022-04-15 20:10:17 +0800

    2020-4-15 ware配置

--- mall-ware/src/test/java/com/zpy/mall/mallware/MallWareApplicationTests.java
+++ mall-ware/src/test/java/com/zpy/mall/mallware/MallWareApplicationTests.java
@@ -1,13 +1,16 @@
 package com.zpy.mall.mallware;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MallWareApplicationTests {
+public class MallWareApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
